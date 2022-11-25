d22ac24b8bcf79912df552ad740bc6dc92815272
1romario1 <115860755+1romario1@users.noreply.github.com>
2022-11-25 09:02:36 -0500

    Delete ciclos3.py

--- Ejercicios con funciones/ciclos3.py
+++
@@ -1,7 +0,0 @@
-def numero_natural(num = int(input("Ingrese el número máximo   "))):
-    cont=0
-    while cont*(cont+1)<=2*num:
-        cont+=1
-        print("El número natural mas pequeño que excede al dato es:", cont)
-numero_natural()        
-    
