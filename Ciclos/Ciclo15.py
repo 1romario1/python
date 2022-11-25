db3299e923bb84219eb40d105de2c587deca3d82
romario <Duglasruz135@gmail.com>
2022-11-25 08:32:34 -0500

    nuevos cambios en Ejercisios romario

--- Ciclos/Ciclo15.py
+++ Ciclos/Ciclo15.py
@@ -1,6 +1,8 @@
 #Diseñar e implementar un programa que solicite a su
 #usuario un valor no negativo n y visualice la siguiente salida:
-
-numero=int(input("ingrese un numero no negativo: "))
-if numero <= -0:
-    print
+i=0
+numero=int(input("ingrese un numero enntero: "))
+print("%i",numero);
+while(numero<=0):
+    if numero <=0:
+        print()
