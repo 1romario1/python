8a3314cae9bf3eca963ae42deed64e9d9f1f62a3
romario <Duglasruz135@gmail.com>
2022-11-25 10:46:21 -0500

    nuevos cambios en Ejercisios romario

--- Ejercisios de listas/lista #10.py
+++ Ejercisios de listas/lista #10.py
@@ -1,5 +1,4 @@
-#Llenar una lista de tamaño aleatorio entre 10 y 25 elementos. Llene la lista con números aleatorios. 
-# Encuentre la mediana de los números de la lista
+#Llenar una lista de tamaño aleatorio entre 10 y 25 elementos. Llene la lista con números aleatorios. Encuentre la desviación estandar
 import random
 tam=round(random.randint(10,25))
 vec=[]
