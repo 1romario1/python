165e0405557119c6250d4eac8172aa3ed21fe740
1romario1 <115860755+1romario1@users.noreply.github.com>
2023-03-03 09:44:41 -0500

    Add files via upload

---
+++ poo/intropoo.py
@@ -0,0 +1,7 @@
+#NO tipado
+x=10#se crea una variable
+x=[]#le asiganmos dos corchetes de lista a nuestra variable
+cad='Amo la programación'#definimos otra variable y le agreamos una cadena de texto
+print(type(cad))#Mostramos por pantalla que tipo de dato es la variable (cad)
+print(len(cad))#Indicamos cuantos indices tiene nuestra variable
+print(cad.capitalize())#le agregamos la funcion capitalize()para poner en mayuscula la primera letra de nuestra cadena de texto
